05b6ed95a16dce237871cce037b3d1dd5e3880ea
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-04-26 19:54:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
 const recordButton = document.getElementById('recordButton');
 const stopButton = document.getElementById('stopButton');
 const audioPlayer = document.getElementById('audioPlayer');
+const downloadButton = document.getElementById('downloadButton');
 
 let mediaRecorder;
 let recordedChunks = [];
 
 recordButton.addEventListener('click', () => {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(stream => {
-            mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.start();
-            recordButton.disabled = true;
-            stopButton.disabled = false;
+  navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+      mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      recordButton.disabled = true;
+      stopButton.disabled = false;
 
-            mediaRecorder.addEventListener('dataavailable', event => {
-                recordedChunks.push(event.data);
-            });
+      mediaRecorder.addEventListener('dataavailable', event => {
+        recordedChunks.push(event.data);
+      });
 
-            mediaRecorder.addEventListener('stop', () => {
-                const audioBlob = new Blob(recordedChunks, { type: 'audio/webm' });
-                const audioURL = URL.createObjectURL(audioBlob);
-                audioPlayer.src = audioURL;
-                recordedChunks = [];
-            });
-        })
-        .catch(error => {
-            console.error('Error accessing audio:', error);
-        });
+      mediaRecorder.addEventListener('stop', () => {
+        const audioBlob = new Blob(recordedChunks, { type: 'audio/webm' });
+        const audioURL = URL.createObjectURL(audioBlob);
+        audioPlayer.src = audioURL;
+        recordedChunks = [];
+        downloadButton.disabled = false;
+      });
+    })
+    .catch(error => {
+      console.error('Error accessing audio:', error);
+    });
 });
 
 stopButton.addEventListener('click', () => {
-    mediaRecorder.stop();
-    recordButton.disabled = false;
-    stopButton.disabled = true;
+  mediaRecorder.stop();
+  recordButton.disabled = false;
+  stopButton.disabled = true;
+});
+
+downloadButton.addEventListener('click', () => {
+  const audioBlob = new Blob(recordedChunks, { type: 'audio/webm' });
+  const audioURL = URL.createObjectURL(audioBlob);
+  const link = document.createElement('a');
+  link.href = audioURL;
+  link.download = `audio-${new Date().toISOString()}.webm`;
+  link.click();
 });
