e0fe6e9d4b92a24d16dace7b95fc0fff98f55fd0
DormantOne <131829942+DormantOne@users.noreply.github.com>
2023-04-30 07:17:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function formatTimestamp(date) {
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const seconds = String(date.getSeconds()).padStart(2, '0');
 
-  return `${year}-${month}-${day}_TIME${hours}:${minutes}:${seconds}`;
+  return `${year}-${month}-${day}_TIME${hours}-${minutes}-${seconds}`;
 }
 
 recordButton.addEventListener('click', () => {
